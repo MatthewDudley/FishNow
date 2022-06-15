2249f1c5fe00fe8b0969a90f89f313414ebc1cfd
Matthew Dudley <md05062@georgiasouthern.edu>
2022-06-15 10:27:54 -0400

    inital commit for adding project files

---
+++ FishNow/FishNow/FishNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishNowApp.swift
+//  FishNow
+//
+//  Created by Matthew Dudley on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FishNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
